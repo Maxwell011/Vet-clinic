60891598e99a4d8145b007f146eb989474709202
Orji Maxwell <chukwuemekaorji678@gmail.com>
2022-12-06 13:56:16 +0000

    Added queries for data

--- queries.sql
+++ queries.sql
@@ -8,3 +8,42 @@ SELECT * FROM animals WHERE weight_kg > 10.4;
 SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+UPDATE animals 
+SET species = 'unspecified';
+
+UPDATE animals  SET species = 'digimon' WHERE name LIKE '%mon';
+
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+BEGIN;
+SAVEPOINT SP1;
+DELETE FROM animals;
+ROLLBACK TO SP1 ;
+COMMIT;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+BEGIN;
+SAVEPOINT SP2;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT SP2;
+UPDATE animals SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(name) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) FROM animals 
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)  FROM animals 
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN 'January 1, 1990' AND 'December 31, 2000'
+GROUP BY species;
+
+
+
